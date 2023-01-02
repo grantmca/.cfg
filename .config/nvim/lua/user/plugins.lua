f70885a34505ef42dd92f10b945d0114b8379ee7
Grant McAllister <grantmca2221@gmail.com>
2023-01-02 17:07:37 -0600

    Added the Chat GPT Plugin:x

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -97,6 +97,20 @@ return packer.startup(function(use)
   -- Git
   use 'lewis6991/gitsigns.nvim'
 
+  use({
+    "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup({
+        -- optional configuration
+      })
+    end,
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  })
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
